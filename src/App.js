13c155dc56fe94f269e4292abcaf4aaf12d4a381
Dani <danifdezalonso@gmail.com>
2021-02-19 19:57:15 +0100

    Last commit de la branch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Filter from "./components/Filter";
 export default function App() {
   return (
     <div className="App">
-      <h1>Test nova branca Ester</h1>
       <Header />
       <Filter />
       <Highlights title="Serveis destacats per la llar" />
