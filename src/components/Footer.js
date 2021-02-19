1d86d4d9a8c986b49c812976863d6dd18e97c790
Dani <danifdezalonso@gmail.com>
2021-02-19 13:58:32 +0100

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,41 @@
 import React from "react";
-import styles from "./Footer.module.scss"; // CSS especific del component
-import "./App.module.scss"; // Reset i CSS general
+import styles from "./Footer.module.scss"; // CSS especific del componentCSS general
 
 const Footer = () => {
   return (
     <>
       <div className={styles.footer}>
         <div className={styles.footer__container}>
-          <div className={styles.footer__description}> Footer description </div>{" "}
           <div className={styles.footer__llistes}>
             <ul className={styles.footer__llistes__links}>
-              <li> Logo </li> <li> Copyright© </li>{" "}
+              <li> Logo </li> <li>Copyright © 2021 WallaServices</li>{" "}
             </ul>{" "}
             <ul className={styles.footer__llistes__links}>
-              <li> List title </li>
-              <li> List </li> <li> List </li> <li> List </li> <li> List </li>{" "}
-              <li> List </li>{" "}
+              <li>
+                <b> WallaServices</b>
+              </li>
+              <li> Qui som? </li> <li> Premsa </li> <li> Feina </li>{" "}
+              <li> Equip </li>{" "}
             </ul>{" "}
             <ul className={styles.footer__llistes__links}>
-              <li> List title </li> <li> List </li> <li> List </li>{" "}
-              <li> List </li> <li> List </li> <li> List </li>{" "}
+              <li>
+                <b> Suport </b>
+              </li>{" "}
+              <li> Preguntes Freqüents </li> <li> Normes de publicació </li>{" "}
+              <li> Consells de seguretat </li>{" "}
             </ul>{" "}
             <ul className={styles.footer__llistes__links}>
-              <li> List title </li> <li> List </li> <li> List </li>{" "}
-              <li> List </li> <li> List </li> <li> List </li>{" "}
+              <li>
+                <b> Legal </b>
+              </li>{" "}
+              <li> Condicions d'ús </li> <li> Política de privacitat</li>{" "}
+              <li> Cookies</li>{" "}
             </ul>{" "}
             <ul className={styles.footer__llistes__links}>
-              <li> List title </li> <li> List </li> <li> List </li>{" "}
-              <li> List </li> <li> List </li> <li> List </li>{" "}
+              <li>
+                <b>Motor </b>
+              </li>{" "}
+              <li> Particulars </li> <li> Professionals </li>{" "}
             </ul>{" "}
           </div>{" "}
         </div>{" "}
