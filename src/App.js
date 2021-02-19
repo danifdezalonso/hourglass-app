837438f8f931a299bcd71ee25d31a346f725562b
Dani <danifdezalonso@gmail.com>
2021-02-19 19:44:00 +0100

    test branch2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Filter from "./components/Filter";
 export default function App() {
   return (
     <div className="App">
+      <h1>Test nova branca</h1>
       <Header />
       <Filter />
       <Highlights title="Serveis destacats per la llar" />
