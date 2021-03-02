44b2525a99a7c2770cc8120e1b0b185d15e5392b
Dani <danifdezalonso@gmail.com>
2021-03-02 11:34:01 +0100

    crida

--- src/components/PublicitatMarqueting.js
+++ src/components/PublicitatMarqueting.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import styles from "./Disseny.module.scss";
+import styles from "./PublicitatMarqueting.module.scss";
+import { db } from "../fire";
+import Servei from "./Servei";
 
 const PublicitatMarqueting = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    db.collection("services")
+      // .where("option.value", == "publicitat")
+      .orderBy("date", "asc")
+      .onSnapshot((snapshot) => {
+        setServices(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            title: doc.data().title,
+            description: doc.data().description,
+            option: doc.data().option.label,
+            optionTag: doc.data().option.value,
+            url: doc.data().url, //linea 39 de AddItem.js
+          }))
+        );
+      });
+  }, []);
+
   return (
     <div className={styles.containerFluid}>
       <div className={styles.container}>
@@ -19,7 +41,15 @@ const PublicitatMarqueting = () => {
         </div>
         <h1 className={styles.filter__header}>Publicitat i Màrqueting</h1>
         <div className={styles.container}>
-          <p>contingut aqui</p>
+          {services.map((service) => (
+            <Servei
+              url={service.url}
+              title={service.title}
+              description={service.description}
+              option={service.option}
+              optionTag={service.optionTag}
+            />
+          ))}
         </div>
       </div>
     </div>
