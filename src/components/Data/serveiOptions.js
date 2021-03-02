29bb59ec1bed34d8d06cbe147bfbebcc74e801a6
esterbvila <ester.batllori@gmail.com>
2021-03-02 18:29:56 +0100

    serveiOptions ordenat abc

--- src/components/Data/serveiOptions.js
+++ src/components/Data/serveiOptions.js
@@ -1,46 +1,31 @@
 const serveiOptions = [
-  // LLAR
-  { value: "neteja", label: "Tasques de neteja" },
+  { value: "cangur", label: "Cangurs" },
+  { value: "consultoria", label: "Consultoria" },
+  { value: "cuina", label: "Classes de cuina" },
+  { value: "dietista", label: "Dietistes" },
+  { value: "disseny", label: "Disseny i Programació" },
   { value: "electricista", label: "Electricistes" },
+  { value: "entrenador", label: "Entrenadors personals" },
+  { value: "facial", label: "Tractaments facials i corporals" },
+  { value: "fotografia ", label: "Fotografia i Video" },
+  { value: "gossos", label: "Passejadors de gossos" },
+  { value: "idiomes", label: "Intercanvi d'idiomes" },
+  { value: "informatic", label: "Informàtics" },
   { value: "jardiner", label: "Jardiners" },
   { value: "lampista", label: "Lampistes" },
-  { value: "pintor", label: "Pintors" },
-  { value: "mudances", label: "Mudances" },
-
-// TECNOLOGIA
-  { value: "informatic", label: "Informàtics" },
-  { value: "disseny", label: "Disseny i Programació" },
-  { value: "publicitat", label: "Publicitat i Màrketing" },
-  { value: "fotografia ", label: "Fotografia i Video" },
-
-// APRENENTATGE
-  { value: "idiomes", label: "Intercanvi d'idiomes" },
-  { value: "cuina", label: "Classes de cuina" },
-  { value: "particulars", label: "Classes particulars" },
-  { value: "musica", label: "Classes de música i ball" },
-
-// BENESTAR I BELLESA
   { value: "massatges", label: "Massatges" },
-  { value: "facial", label: "Tractaments facials i corporals" },
+  { value: "mecanic", label: "Mecànics" },
+  { value: "mudances", label: "Mudances" },
+  { value: "musica", label: "Classes de música i ball" },
+  { value: "neteja", label: "Tasques de neteja" },
+  { value: "particulars", label: "Classes particulars" },
   { value: "perruqueria", label: "Perruquers" },
-  { value: "psicologia", label: "Psicologia" },
-  { value: "dietista", label: "Dietistes" },
-  { value: "entrenador", label: "Entrenadors personals" },
+  { value: "pintor", label: "Pintors" },
+  { value: "psicologia", label: "Psicologia" }, 
+  { value: "publicitat", label: "Publicitat i Màrketing" },
   { value: "terapies", label: "Terapies Alternatives" },
-
-// CONSULTORIA
-  { value: "consultoria", label: "Consultoria" },
-
-// ALTRES
-{ value: "gossos", label: "Passejadors de gossos" },
-{ value: "cangur", label: "Cangurs" },
-{ value: "mecanic", label: "Mecànics" },
-
-{ value: "altres", label: "Altres" },
-,
+  { value: "altres", label: "Altres" },
 ];
-
-//25 categories
 
 export default serveiOptions;
 
